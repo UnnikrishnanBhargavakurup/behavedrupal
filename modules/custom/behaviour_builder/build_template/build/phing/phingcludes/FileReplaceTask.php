0836fce18a3c9f6398331441a98c3202eba44a70
unnikrishnan <unnikrishnan.b@landmarkgroup.in>
2016-09-17 14:59:58 +0530

    fixes in coding standards and added screenshot path in behat configuration.

--- modules/custom/behaviour_builder/build_template/build/phing/phingcludes/FileReplaceTask.php
+++ modules/custom/behaviour_builder/build_template/build/phing/phingcludes/FileReplaceTask.php
@@ -4,20 +4,21 @@ require_once "phing/Task.php";
 
 class FileReplaceTask extends Task {
 
-    protected $browser = null;
-    protected $file = null;
+  protected $browser = null;
+  
+  protected $file = null;
 
-    public function main() {
-      $file_contents = file_get_contents($this->file);
-      $file_contents = preg_replace("/browser_name:.*$/m", 'browser_name: '. $this->browser, $file_contents);
-      file_put_contents($this->file, $file_contents);
-    }
+  public function main() {
+    $file_contents = file_get_contents($this->file);
+    $file_contents = preg_replace("/browser_name:.*$/m", 'browser_name: '. $this->browser, $file_contents);
+    file_put_contents($this->file, $file_contents);
+  }
 
-    public function setBrowser($browser) {
-      $this->browser = "" . $browser;
-    }
+  public function setBrowser($browser) {
+    $this->browser = "" . $browser;
+  }
 
-    public function setFile($file) {
-      $this->file = "" . $file;
-    }
+  public function setFile($file) {
+    $this->file = "" . $file;
+  }
 }
